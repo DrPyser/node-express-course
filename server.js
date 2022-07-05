0282269a769eac68afa39b48441edfd822e95ccf
Charles <charles.langlois@accenture.com>
2022-07-05 13:11:19 -0400

    First GET endpoint

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 
+const mockUsers = [
+  { name: 'Mark' },
+  { name: 'Jill' }
+]
+
+app.get('/users', (req, res) => res.json({
+  success: true,
+  message: 'successfully got users. Nice!',
+  users: mockUsers
+}))
+
 app.listen(8000, () => {
   console.log('server is running!')
 })
